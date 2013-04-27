51fe0e49111da0543fdce5a5835a682d31a12811
Sam Macpherson <sam.mack91@gmail.com>
2013-04-28 06:41:38 +1000

    moved eventq module into new branch (eventmgr), new module interface and todo list

---
+++ eventq.h
@@ -0,0 +1,18 @@
+#ifndef _EVENTQ_H_
+#define _EVENTQ_H_
+
+#include "stdtypes.h"
+
+#define EVENTQSIZE 32
+
+typedef void (*func_t)(void*);
+
+void vEventQRegisterEvent(func_t fFunc, void* vpData, uint64_t ulTickCount);
+
+void vEventQStartOneShot(func_t fFunc, void* vpData, uint64_t ulPeriodus);
+
+void vEventQStartPeriodic(func_t fFunc, void* vpData, uint64_t ulPeriodus);
+
+size_t ulEventQGetSize(void);
+
+#endif
