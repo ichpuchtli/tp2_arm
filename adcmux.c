2cec9c25c3cd3f9d22aceca13d221eb30d7388f5
Sam Macpherson <sam.mack91@gmail.com>
2013-04-30 08:27:55 +1000

    half finished adcmux module, pop from a stash before I forget

--- adcmux.c
+++ adcmux.c
@@ -1,9 +1,85 @@
 #include "adcmux.h"
+#include "driverlib/adc.h"
 
-void vADCMuxInit(void){}
+void vADCMuxInit(void){
 
-void vADCMuxSelect(uint8_t ucChannel){}
 
-void vADCMuxTrigger(void){}
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 
-uint16_t usADCMuxGetValue(void){return (uint16_t)(0);}
+    // TODO: change this to whichever GPIO port you are using.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    //
+    // Select the analog ADC function for these pins.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_7);
+
+    //
+    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+    // will do a single sample when the processor sends a signal to start the
+    // conversion.  Each ADC module has 4 programmable sequences, sequence 0
+    // to sequence 3.  This example is arbitrarily using sequence 3.
+    //
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+    //
+    // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
+    // single-ended mode (default) and configure the interrupt flag
+    // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
+    // that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
+    // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
+    // sequence 0 has 8 programmable steps.  Since we are only doing a single
+    // conversion using sequence 3 we will only configure step 0.  For more
+    // information on the ADC sequences and steps, reference the datasheet.
+    //
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+                             ADC_CTL_END);
+
+    //
+    // Since sample sequence 3 is now configured, it must be enabled.
+    //
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+    //
+    // Clear the interrupt status flag.  This is done to make sure the
+    // interrupt flag is cleared before we sample.
+    //
+    ADCIntClear(ADC0_BASE, 3);
+
+}
+
+void vADCMuxSelect(uint8_t ucChannel){
+    g_ucADCMuxChannel = ucChannel;
+}
+
+void vADCMuxTrigger(void){
+
+    ADCProcessorTrigger(ADC0_BASE, 3);
+}
+
+uint32_t ulADCMuxGetValue(void){
+    
+    //
+    // This array is used for storing the data read from the ADC FIFO. It
+    // must be as large as the FIFO for the sequencer in use.  This example
+    // uses sequence 3 which has a FIFO depth of 1.  If another sequence
+    // was used with a deeper FIFO, then the array size must be changed.
+    //
+    unsigned long ulADC0_Value[1];
+
+    // Wait for conversion to be completed.
+    while(!ADCIntStatus(ADC0_BASE, 3, false))
+    {
+    }
+
+    // Clear the ADC interrupt flag.
+    ADCIntClear(ADC0_BASE, 3);
+
+    // Read ADC Value.
+    ADCSequenceDataGet(ADC0_BASE, 3, ulADC0_Value);
+
+    return ulADC0_Value[0];
+}
